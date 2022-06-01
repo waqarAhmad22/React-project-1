fe242257469936aadf812f8c409eacc9211041c3
waqarAhmad22 <106268105+waqarAhmad22@users.noreply.github.com>
2022-06-01 14:55:26 +0500

    react

---
+++ src/getstart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./navbar.css"
+export default function getstart() {
+  return (
+    <div>
+        <button className='style1'>get started</button>
+        </div>
+  )
+}
